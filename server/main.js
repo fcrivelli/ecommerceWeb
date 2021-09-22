c8d8e137f21dd25b74cba358033093d4acf60bab
fcrivelli <cofrta@gmail.com>
2021-09-22 20:41:23 -0300

    sqlite

--- server/main.js
+++ server/main.js
@@ -1,16 +1,59 @@
+//Librerias y dependencias
 var express = require('express'); 
-var app = express(); 
-var server = require('http').Server(app); 
-var io = require('socket.io')(server);
+var app = express();
+const PORT = 3307;
+const server = require('http').Server(app); 
+const io = require('socket.io')(server);
+const { options } = require('../options/sqlite3');
+const knex = require('knex')(options); 
 
-var products = [];
-var messages = []; 
+//Recursos
+app.use(express.static(__dirname));
 
-app.use(express.static('public')); 
-app.get('/hello', function(req, res) {
-  res.status(200).send("Hello World!");
-});
+//Configuracion del servidor
+app.set('views', './views'); //permite gestionar las rutas de los diferentes recursos
+app.set('view engine','ejs'); //Establece el motor de plantilla, con archivos ejs
+app.use(express.urlencoded({extended: true})); //Permiten recuperar valores publicados en un request
 
+//Base de Datos
+//Creacion Tablas
+knex.schema.hasTable('producto').then(exists => {
+    if(exists){
+        knex.destroy();
+    } else {
+        knex.schema.createTable('producto', table => {
+            table.increments('id')
+            table.string('name')
+            table.integer('price')
+            table.string('thumbnail')
+        }).then(() => console.log("table created"))
+        .catch((err) => { console.log(err); throw err })
+        .finally(() => {
+            knex.destroy();
+        })
+        knex.destroy();
+    }
+})
+
+knex.schema.hasTable('cart').then(exists => {
+    if(exists){
+        knex.destroy();
+    } else {
+        knex.schema.createTable('cart', table => {
+            table.increments('id')
+            table.string('name')
+            table.integer('amount')
+            table.integer('productoid')
+        }).then(() => console.log("table created"))
+        .catch((err) => { console.log(err); throw err })
+        .finally(() => {
+            knex.destroy();
+        })
+    }
+})
+
+
+//Coneccion web socket para productos
 io.on('connection', function(socket) { 
   console.log('Alguien se ha conectado con Sockets');   
   socket.emit('products', products); 
@@ -20,6 +63,7 @@ io.on('connection', function(socket) {
   }); 
 });
 
+//Coneccion web socket para mensajes usuarios
 io.on('connection', function(socket) { 
   console.log('Alguien se ha conectado con Sockets');   
   socket.emit('messages', messages); 
@@ -27,8 +71,98 @@ io.on('connection', function(socket) {
     messages.push(data); 
     io.sockets.emit('messages', messages); 
   }); 
-}); 
- 
-server.listen(8080, function() { 
-  console.log("Servidor corriendo en http://localhost:8080"); 
 });
+
+//Enrutamiento
+//Inicio de pagina
+app.get('/', (req, res) => {
+    res.render("./layouts/main.ejs");
+})
+
+//Adherir productos
+app.get('/crear-producto', (req, res) => {
+    res.render("./layouts/addProduct.ejs");
+})
+
+app.post('/crear-producto', (req, res) => {
+
+})
+
+//Muestra cartilla
+app.get('/cartilla', (req, res) => {
+  res.render("./layouts/cart.ejs");
+})
+
+//Muestra listado de productos
+app.get('/productos', (req, res) => {
+    knex.from('producto').select("*").orderBy('name')
+    .then((rows) => {
+        res.render('./layouts/productos.ejs', {modelo: rows} );
+    }).catch((err) => { console.log(err); throw err; })
+    .finally(() => {
+        knex.destroy();
+    })
+})
+
+//Muestra listado de productos
+app.get('/contacto', (req, res) => {
+    res.render("./layouts/contact.ejs");
+})
+
+app.get('/*', (req, res) =>{
+    res.render('./layouts/notfound.ejs');
+})
+
+//Abro mi servidor
+app.listen(PORT, err => {
+    if(err) throw new Error(`Error en servidor ${err}`)
+    console.log("My HTTP server listening on port " + PORT + "...");
+});
+
+/*
+app.get('/api/productos/listar', (req, res) => {
+    if(indice == 0){
+        res.status(400).send(msjProductsNotCharge);
+    } else {
+        res.send(workProducts.getProducts());
+    }
+});
+
+app.get('/api/productos/listar/:id', (req, res) => {
+    if(!workProducts.validateId(req.params.id)){
+        res.status(400).send(msjProductNotFound);
+    } else {
+        res.send({
+            id: req.params.id,
+            product: workProducts.getProductById(req.params.id)
+        });
+    }
+});
+
+app.delete('/api/productos/borrar', (req, res) => {
+    if(!workProducts.validateBody(req.body)){
+        res.status(400).send(msjError);
+    } else {
+        workProducts.deleteProduct(req.body);
+        res.send(msjDelete);
+    }
+});
+
+app.put('/api/productos/actualizar', (req, res) => {
+    if(!workProducts.validateBody(req.body)){
+        res.status(400).send(msjError);
+    } else {
+        workProducts.updateProduct(req.body);
+        res.send(msjSave);
+    }
+});
+
+app.post('/api/productos/guardar', (req, res) => {
+    if(!workProducts.validateBody(req.body)){
+        res.status(400).send(msjError);
+    } else {
+        workProducts.saveProduct(req.body);
+        res.send(msjSave);
+    }
+});
+*/
